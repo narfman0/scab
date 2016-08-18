a0ec5575a82f48cc6795c30ee66f087ab06e393c
Jon Robison <narfman0@gmail.com>
2016-08-17 23:12:18 -0400

    -Change protobuf dependency -Move tests to run properly

---
+++ src/test/java/com/blastedstudios/scab/world/being/NPCTest.java
@@ -0,0 +1,21 @@
+package com.blastedstudios.scab.world.being;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.blastedstudios.scab.world.being.NPC.DifficultyEnum;
+
+public class NPCTest {
+	@Test public void testShootDelay() {
+		assertEquals(5000f, NPC.shootDelay(1, DifficultyEnum.HARD), 500f);
+		assertEquals(3000f, NPC.shootDelay(10, DifficultyEnum.HARD), 500f);
+		assertEquals(2000f, NPC.shootDelay(50, DifficultyEnum.HARD), 200f);
+		assertEquals(1000f, NPC.shootDelay(100, DifficultyEnum.HARD), 100f);
+		
+		assertEquals(7000f, NPC.shootDelay(1, DifficultyEnum.MEDIUM), 500f);
+		assertEquals(5000f, NPC.shootDelay(10, DifficultyEnum.MEDIUM), 500f);
+		assertEquals(4000f, NPC.shootDelay(50, DifficultyEnum.MEDIUM), 200f);
+		assertEquals(3000f, NPC.shootDelay(100, DifficultyEnum.MEDIUM), 100f);
+	}
+}
